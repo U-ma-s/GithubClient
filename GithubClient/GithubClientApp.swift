1980705713b06657284f8479fc787ddc61bb638b
U-ma-s <yuma.prodia4989@gmail.com>
2023-03-04 14:53:21 +0900

    Initial Commit

---
+++ GithubClient/GithubClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubClientApp.swift
+//  GithubClient
+//
+//  Created by 杉下友麻 on 2023/03/04.
+//
+
+import SwiftUI
+
+@main
+struct GithubClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
